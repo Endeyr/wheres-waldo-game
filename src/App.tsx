a42af193a0755e5dbba16181aabf2fb98b2f0953
Endeyr <aaron.thompson42594@gmail.com>
2024-05-22 09:28:48 -0400

    mvp wheres waldo game

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import CanvasComponent from "./components/canvas";
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [waldoFound, setWaldoFound] = useState(false);
+  const waldoPosition = { x: 780, y: 264, width: 20, height: 30 };
+
+  const handleCanvasClick = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  ) => {
+    const canvas = e.target as HTMLCanvasElement;
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    if (
+      x >= waldoPosition.x &&
+      x <= waldoPosition.x + waldoPosition.width &&
+      y >= waldoPosition.y &&
+      y <= waldoPosition.y + waldoPosition.height
+    ) {
+      setMessage("You found Waldo!");
+      setWaldoFound(true);
+    } else {
+      setMessage("Try again.");
+      setWaldoFound(false);
+    }
+  };
   return (
     <>
-      <div>Hello from Home</div>
+      <div className="flex flex-col items-center justify-center p-2 m-2">
+        <h1>Where's Waldo Game</h1>
+        <CanvasComponent
+          onClick={handleCanvasClick}
+          waldoFound={waldoFound}
+          waldoPosition={waldoPosition}
+        />
+        <p>{message}</p>
+      </div>
     </>
   );
 }
---
+++ src/components/canvas/index.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef } from "react";
+import waldoImage from "../../assets/wheres-waldo.webp";
+const CanvasComponent = ({
+  onClick,
+  waldoPosition,
+  waldoFound,
+}: {
+  onClick: (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => void;
+  waldoPosition: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  waldoFound: boolean;
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      const canvas = canvasRef.current;
+      if (canvas) {
+        const ctx = canvas.getContext("2d");
+        const img = new Image();
+        img.src = waldoImage;
+        if (ctx) {
+          img.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            if (waldoFound) {
+              ctx.strokeStyle = "black";
+              ctx.lineWidth = 5;
+              ctx.strokeRect(
+                waldoPosition.x,
+                waldoPosition.y,
+                waldoPosition.width,
+                waldoPosition.height,
+              );
+            }
+          };
+        }
+      }
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [waldoFound, waldoPosition]);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      console.log(true);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return (
+    <canvas
+      ref={canvasRef}
+      width={1280}
+      height={720}
+      onClick={onClick}
+      style={{ border: "1px solid black" }}
+    />
+  );
+};
+export default CanvasComponent;
